f605cc5d4c1e33be4a1e9818489745a30b7d0852
teles <josetelesmaciel@gmail.com>
2017-10-05 20:07:10 -0300

    testa filename

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-let simpleMDE;
+let simpleMDE
 
 const getHash = () => {
-    return window.location.hash.indexOf("#!") !== -1 ? window.location.hash.toString().replace("#!", "") : window.location.pathname;
+  return window.location.hash.indexOf('#!') !== -1 ? window.location.hash.toString().replace('#!', '') : window.location.pathname
 }
 const hash = getHash();
 
-const JSONToParams = function params(obj){
+const JSONToParams = function params (obj) {
   return Object.keys(obj).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&')
-};
+}
 
 const updateDontpad = () => {
-  fetch(`http://dontpad.com${hash}` , {
-    body: JSONToParams({"text":simpleMDE.value()}),
-    mode: "no-cors",
+  fetch(`http://dontpad.com${hash}`, {
+    body: JSONToParams({'text': simpleMDE.value()}),
+    mode: 'no-cors',
     headers: new Headers({
-      "Accept": "application/json",
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     }),
-    method: "POST"
-  }).then(function(res){
-    console.log(res);
-  });
-};
+    method: 'POST'
+  }).then(function (res) {
+    console.log(res)
+  })
+}
 
-var timeout = setTimeout( () => {
+var timeout = setTimeout(() => {
   simpleMDE = new SimpleMDE({
-    element: document.querySelector("[data-selector='markpad-textarea']"),
+    element: document.querySelector('[data-selector=\'markpad-textarea\']'),
     autofocus: true,
     toolbar: [
-      "bold", "italic", "heading", "|" , "quote", "unordered-list", "ordered-list", "|",
-      "link", "image", "|", "preview", "side-by-side", {
-        "name": "save",
-        "action": () => {
-        var file = new File([simpleMDE.value()], `${hash}.txt`, {type: "text/plain;charset=utf-8"});
-saveAs(file);
-},
-"className": "fa fa-download p-float-right",
-  "title": "Save file"
-}
-]
-});
-simpleMDE.toggleFullScreen();
-}, 0);
+      'bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|',
+      'link', 'image', '|', 'preview', 'side-by-side', {
+        'name': 'save',
+        'action': () => {
+          var filename = `${hash}.txt`;
+          console.log(filename);
+          var file = new File([simpleMDE.value()], filename, {type: 'text/plain;charset=utf-8'})
+          saveAs(file)
+        },
+        'className': 'fa fa-download p-float-right',
+        'title': 'Save file'
+      }
+    ]
+  })
+  simpleMDE.toggleFullScreen()
+}, 0)
 
-var intervalShowMDEContent = setInterval( () => {
-  updateDontpad();
-}, 2000);
+var intervalShowMDEContent = setInterval(() => {
+  updateDontpad()
+}, 2000)
