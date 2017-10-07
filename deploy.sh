edc4a953533990c8ddd7c0ba1a47d341b41aecc6
teles <josetelesmaciel@gmail.com>
2017-10-06 23:22:29 -0300

    sem-issue - cria script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+dir=DISC_CLONED
+git clone https://github.com/markpad/markpad.git $dir
+cd $dir
+git checkout master
+sed --in-place '/\/build\.js/d' .gitignore || exit 0
+git rm build.js
+cat ./.gitignore
+npm install
+npm run build
+git add build.js
+ls
+git commit -m "Atualizando gh-pages via travis-ci"
+git status
+git push --force "${GH_REFERENCE}" master:gh-pages
