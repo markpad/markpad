f047b2f1dd5fba1050fcbf8b890838b378de318f
José Teles <josetelesmaciel@gmail.com>
2026-03-08 16:51:50 -0300

    refactor(utils): improve debounce utility with generic types

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,8 +1,15 @@
-function debounce (func: (...args: string[]) => void, delay: number): () => void {
+type AnyFunction = (...args: any[]) => any
+
+export function debounce<T extends AnyFunction>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
   let timeoutId: ReturnType<typeof setTimeout>
-  return function (...args: string[]) {
+  return function (...args: Parameters<T>) {
     clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => { func.apply(null, args) }, delay)
+    timeoutId = setTimeout(() => {
+      func.apply(null, args)
+    }, delay)
   }
 }
 
